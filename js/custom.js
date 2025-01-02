d68e270e55000b9619455b566d4d3b62a4ec533e
mkhanshar <muhammadkhanpps@outlook.com>
2025-01-02 09:55:36 +0500

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,26 @@
+  $(".slick-slider").slick({
+   slidesToShow: 3,
+   infinite:true,
+   slidesToScroll: 1,
+   autoplay: true,
+   autoplaySpeed: 2000,
+  });
+
+
+
+  $(".slick-slider1").slick({
+   slidesToShow: 3,
+   infinite:true,
+   slidesToScroll: 1,
+   autoplay: true,
+   autoplaySpeed: 2000,
+  });
+
+
+  $(".slick-slider2").slick({
+   slidesToShow: 1,
+   infinite:true,
+   slidesToScroll: 1,
+   autoplay: true,
+   autoplaySpeed: 2000,
+  });
